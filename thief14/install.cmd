9f56215a80951bd5886f61145757e9ec62326c16
Norbert Navratil <131018336+nnorbik@users.noreply.github.com>
2024-03-15 17:00:02 +0100

    Soubory k českému překladu hry Thief The Dark Project a Thief Gold

---
+++ thief14/install.cmd
@@ -0,0 +1,7 @@
+del BITMAP.CRF
+del DEFAULT.CRF
+del EDITOR.CRF
+del MESH.CRF
+del MOTIONS.CRF
+del PAL.CRF
+del SND.CRF
---
+++ thief33/install.cmd
@@ -0,0 +1,8 @@
+rmdir /s /q intrface
+del BITMAP.CRF
+del DEFAULT.CRF
+del EDITOR.CRF
+del MESH.CRF
+del MOTIONS.CRF
+del PAL.CRF
+del SND.CRF
